9909ae1b2a237ac3040706596b8608c5fc92fdca
David Llobell <dllobellmoya@gmail.com>
2024-10-11 19:09:47 +0200

    Fix contacts read response import

--- src/Resources/Contacts.php
+++ src/Resources/Contacts.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Eolica\Hubspot\Resources;
 
 use Eolica\Hubspot\Http\Response;
-use Eolica\Hubspot\Resources\Deals\ReadResponse;
+use Eolica\Hubspot\Resources\Contacts\ReadResponse;
 
 final readonly class Contacts extends Resource
 {
@@ -22,7 +22,7 @@ final readonly class Contacts extends Resource
         ?bool $archived = null,
         ?string $idProperty = null,
     ): ReadResponse {
-        /** @var Response<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool, associations: array<string, array{results: array<array<string, string>>}>|null}>> */
+        /** @var Response<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool, associations: array<string, array{results: array<array<string, string>>}>|null}> */
         $response = $this->transporter->get("/crm/v3/objects/contacts/{$id}", [
             'properties' => $this->parseListParameter($properties),
             'propertiesWithHistory' => $this->parseListParameter($propertiesWithHistory),
