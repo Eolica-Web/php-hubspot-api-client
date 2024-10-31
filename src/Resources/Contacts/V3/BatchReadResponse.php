1794e572a188a145a839f0d2a64b699761a11fcc
David Llobell <dllobellmoya@gmail.com>
2024-10-31 10:17:07 +0100

    Add contacts batch read method

---
+++ src/Resources/Contacts/V3/BatchReadResponse.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Eolica\Hubspot\Resources\Contacts\V3;
+
+use Eolica\Hubspot\Http\Meta;
+use Eolica\Hubspot\Http\Response;
+
+final readonly class BatchReadResponse
+{
+    /**
+     * @param array<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool}> $results
+     */
+    private function __construct(
+        public string $status,
+        public array $results,
+        public string $startedAt,
+        public string $completedAt,
+        public Meta $meta,
+    ) {}
+
+    /**
+     * @param Response<array{status: string, results: array<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool}>, startedAt: string, completedAt: string}> $response
+     */
+    public static function fromResponse(Response $response): self
+    {
+        return new self(
+            $response->data['status'],
+            $response->data['results'],
+            $response->data['startedAt'],
+            $response->data['completedAt'],
+            $response->meta,
+        );
+    }
+}
--- src/Resources/Contacts/V3/Contacts.php
+++ src/Resources/Contacts/V3/Contacts.php
@@ -46,4 +46,28 @@ final readonly class Contacts extends Resource
 
         return UpdateResponse::fromResponse($response);
     }
+
+    /**
+     * @param list<array<string, string>> $inputs
+     * @param array<string>|null $properties
+     * @param array<string>|null $propertiesWithHistory
+     */
+    public function batchRead(
+        array $inputs,
+        ?array $properties = null,
+        ?array $propertiesWithHistory = null,
+        ?bool $archived = null,
+        ?string $idProperty = null,
+    ): BatchReadResponse {
+        /** @var Response<array{status: string, results: array<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool}>, startedAt: string, completedAt: string}> */
+        $response = $this->transporter->post('/crm/v3/objects/contacts/batch/read', [
+            'inputs' => $inputs,
+            'properties' => $properties,
+            'propertiesWithHistory' => $propertiesWithHistory,
+            'archived' => $archived,
+            'idProperty' => $idProperty,
+        ]);
+
+        return BatchReadResponse::fromResponse($response);
+    }
 }
