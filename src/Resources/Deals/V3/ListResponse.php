3a410440374924c1a0e4c7d30e4cde61b3b580e3
David Llobell <dllobellmoya@gmail.com>
2025-12-12 12:49:46 +0100

    Make deals list response paging property nullable

--- src/Resources/Deals/V3/ListResponse.php
+++ src/Resources/Deals/V3/ListResponse.php
@@ -11,22 +11,22 @@ final readonly class ListResponse
 {
     /**
      * @param array<array{id: string, properties: array<string, string>}> $results
-     * @param array{next: array{after: string, link: string}} $paging
+     * @param array{next: array{after: string, link: string}}|null $paging
      */
     private function __construct(
         public array $results,
-        public array $paging,
+        public ?array $paging,
         public Meta $meta,
     ) {}
 
     /**
-     * @param Response<array{results: array<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool}>, paging: array{next: array{after: string, link: string}}}> $response
+     * @param Response<array{results: array<array{id: string, properties: array<string, string>, createdAt: string, updatedAt: string, archived: bool}>, paging?: array{next: array{after: string, link: string}}}> $response
      */
     public static function fromResponse(Response $response): self
     {
         return new self(
             $response->data['results'],
-            $response->data['paging'],
+            $response->data['paging'] ?? null,
             $response->meta,
         );
     }
